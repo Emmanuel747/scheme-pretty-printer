ec78187f8c989587040dfd581fbe8277d0b55d4e
Emmanuel Ejim <emmanueleejim@gmail.com>
2022-10-07 22:21:05 -0500

    fix indentation

--- Parse/Parser.java
+++ Parse/Parser.java
@@ -41,76 +41,76 @@ import Tokens.TokenType;
 import Tree.*;
 
 public class Parser {
-    private Scanner scanner;
+  private Scanner scanner;
 
-    public Parser(Scanner s) {
-        scanner = s;
+  public Parser(Scanner s) {
+      scanner = s;
+  }
+
+  public Node parseExp() {
+      // TODO: write code for parsing an exp
+      Token token = scanner.getNextToken();
+      if(token == null){
+          return null;
+      }
+      else{
+          return parseExp(token);
+      }
+  }
+
+  private Node parseExp(Token token){
+      if (token.getType() == TokenType.LPAREN){
+    return parseRest();
+  }
+      else if (token.getType() == TokenType.IDENT){
+    return new Ident(token.getName());
+  }
+      else if (token.getType() == TokenType.QUOTE){
+    return new Cons(new Ident("quote"), new Cons(parseExp(), new Nil()));
+      }
+      else if (token.getType() == TokenType.TRUE){
+    return new BooleanLit(true);
+  }
+  else if (token.getType() == TokenType.FALSE){
+    return new BooleanLit(false);
+  }
+  else if (token.getType() == TokenType.INT){
+    return new IntLit(token.getIntVal());
+  }
+  else if (token.getType() == TokenType.STRING){
+    return new StrLit(token.getStrVal());
+  }
+  else{
+          System.err.print("parseExp Error");
+    return null;
+  }
+  }
+
+  protected Node parseRest() {
+      // TODO: write code for parsing rest
+      Token token = scanner.getNextToken();
+      if(token == null){
+          return null;
+      }
+      else{
+          return parseRest(token);
+      }
+  }
+
+  private Node parseRest(Token token){
+    if (token.getType() == TokenType.RPAREN)
+    {
+      return new Nil();
     }
-
-    public Node parseExp() {
-        // TODO: write code for parsing an exp
-        Token token = scanner.getNextToken();
-        if(token == null){
-            return null;
-        }
-        else{
-            return parseExp(token);
-        }
+    else if (token.getType() == TokenType.DOT)
+    {
+      return new Cons(parseExp(), parseRest());
     }
-
-    private Node parseExp(Token token){
-        if (token.getType() == TokenType.LPAREN){
-			return parseRest();
-		}
-        else if (token.getType() == TokenType.IDENT){
-			return new Ident(token.getName());
-		}
-        else if (token.getType() == TokenType.QUOTE){
-			return new Cons(new Ident("quote"), new Cons(parseExp(), new Nil()));
-        }
-        else if (token.getType() == TokenType.TRUE){
-			return new BooleanLit(true);
-		}
-		else if (token.getType() == TokenType.FALSE){
-			return new BooleanLit(false);
-		}
-		else if (token.getType() == TokenType.INT){
-			return new IntLit(token.getIntVal());
-		}
-		else if (token.getType() == TokenType.STRING){
-			return new StrLit(token.getStrVal());
-		}
-		else{
-            System.err.print("parseExp Error");
-			return null;
-		}
+    else
+    {
+      return new Cons(parseExp(), parseRest());
     }
+  }
 
-    protected Node parseRest() {
-        // TODO: write code for parsing rest
-        Token token = scanner.getNextToken();
-        if(token == null){
-            return null;
-        }
-        else{
-            return parseRest(token);
-        }
-    }
-
-    private Node parseRest(Token token){
-			if (token.getType() == TokenType.RPAREN)
-			{
-				return new Nil();
-			}
-			else if (token.getType() == TokenType.DOT)
-			{
-				return new Cons(parseExp(), parseRest());
-			}
-			else
-			{
-				return new Cons(parseExp(), parseRest());
-			}
-    }
-
-    // TODO: Add any additional methods you might need.
+  // TODO: Add any additional methods you might need.
 }
